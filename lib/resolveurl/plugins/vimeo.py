7a24aab6f436fbd25c78c57b0ac88afa5cdde66d
gujal <gujal@protonmail.com>
2020-06-03 18:26:35 +1200

    next batch

--- lib/resolveurl/plugins/vimeo.py
+++ lib/resolveurl/plugins/vimeo.py
@@ -1,6 +1,6 @@
 """
-    plugin in for ResolveUrl
-    Copyright (C) 2019 gujal
+    Plugin for ResolveURL
+    Copyright (C) 2011 t0mm0
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -16,29 +16,33 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import re
-from lib import helpers
+import json
+from resolveurl.plugins.lib import helpers
 from resolveurl import common
 from resolveurl.resolver import ResolveUrl, ResolverError
 
-class ViuclipsResolver(ResolveUrl):
-    name = "viuclips"
-    domains = ["viuclips.net", "veuclips.com"]
-    pattern = r'(?://|\.)(v[ie]uclips\.(?:net|com))/(?:embed/)?([0-9a-zA-Z]+)'
+
+class VimeoResolver(ResolveUrl):
+    name = "vimeo"
+    domains = ["vimeo.com", "player.vimeo.com"]
+    pattern = r'(?://|\.)(vimeo\.com)/(?:video/)?([0-9a-zA-Z]+)'
 
     def __init__(self):
         self.net = common.Net()
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.FF_USER_AGENT}
-        response = self.net.http_GET(web_url, headers=headers)
-        html = response.content
-        if 'video has been removed' not in html:
-            source = re.findall('hls:"([^"]+)', html)[0]
-            return source + helpers.append_headers({'User-Agent': common.FF_USER_AGENT})
+        headers = {'User-Agent': common.FF_USER_AGENT,
+                   'Referer': 'https://vimeo.com/',
+                   'Origin': 'https://vimeo.com'}
+        html = self.net.http_GET(web_url, headers).content
+        data = json.loads(html)
+        sources = [(vid['height'], vid['url']) for vid in data.get('request', {}).get('files', {}).get('progressive', {})]
+        if sources:
+            sources.sort(key=lambda x: x[0], reverse=True)
+            return helpers.pick_source(sources)
 
         raise ResolverError('File Not Found or removed')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://player.viuclips.net/embed/{media_id}')
+        return self._default_get_url(host, media_id, template='https://player.vimeo.com/video/{media_id}/config')
--- lib/resolveurl/plugins/viuclips.py
+++ lib/resolveurl/plugins/viuclips.py
@@ -1,6 +1,6 @@
 """
-    resolveurl XBMC Addon
-    Copyright (C) 2011 t0mm0
+    Plugin for ResolveUrl
+    Copyright (C) 2019 gujal
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -15,28 +15,31 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-import json
-from lib import helpers
+
+import re
+from resolveurl.plugins.lib import helpers
 from resolveurl import common
 from resolveurl.resolver import ResolveUrl, ResolverError
 
-class VimeoResolver(ResolveUrl):
-    name = "vimeo"
-    domains = ["vimeo.com"]
-    pattern = '(?://|\.)(vimeo\.com)/(?:video/)?([0-9a-zA-Z]+)'
+
+class ViuclipsResolver(ResolveUrl):
+    name = "viuclips"
+    domains = ["viuclips.net", "veuclips.com"]
+    pattern = r'(?://|\.)(v[ie]uclips\.(?:net|com))/(?:embed/)?([0-9a-zA-Z]+)'
 
     def __init__(self):
         self.net = common.Net()
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
-        headers = {'Referer': 'https://vimeo.com/', 'Origin': 'https://vimeo.com'}
-        data = self.net.http_GET(web_url, headers).content
-        data = json.loads(data)
-        sources = [(vid['height'], vid['url']) for vid in data.get('request', {}).get('files', {}).get('progressive', {})]
-        try: sources.sort(key=lambda x: x[0], reverse=True)
-        except: pass
-        return helpers.pick_source(sources)
+        headers = {'User-Agent': common.FF_USER_AGENT}
+        response = self.net.http_GET(web_url, headers=headers)
+        html = response.content
+        if 'video has been removed' not in html:
+            source = re.findall('hls:"([^"]+)', html)[0]
+            return source + helpers.append_headers({'User-Agent': common.FF_USER_AGENT})
+
+        raise ResolverError('File Not Found or removed')
 
     def get_url(self, host, media_id):
-        return 'https://player.vimeo.com/video/%s/config' % media_id
+        return self._default_get_url(host, media_id, template='https://player.viuclips.net/embed/{media_id}')
