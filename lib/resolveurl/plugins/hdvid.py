891e00d34f79bb3292db70f4b7df6e2d7d92760c
Cool Gujal <coolgujal@gmail.com>
2020-06-04 15:35:28 -0400

    fixes 6/2

--- lib/resolveurl/plugins/hdvid.py
+++ lib/resolveurl/plugins/hdvid.py
@@ -1,4 +1,4 @@
-'''
+"""
     resolveurl XBMC Addon
     Copyright (C) 2016 Gujal
 
@@ -14,18 +14,34 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
-'''
+"""
+
 from resolveurl.plugins.lib import helpers
-from resolveurl.resolver import ResolveUrl, ResolverError  # @Unused import
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
 
 
 class HDvidResolver(ResolveUrl):
     name = 'HDvid'
     domains = ['hdvid.tv']
-    pattern = r'(?://|\.)(hdvid\.tv)/(?:embed-)?([0-9a-zA-Z]+)'
+    pattern = r'(?://|\.)(hdvid\.(?:tv|fun))/(?:embed-)?([0-9a-zA-Z]+)'
+
+    def __init__(self):
+        self.net = common.Net()
 
     def get_media_url(self, host, media_id):
-        return helpers.get_media_url(self.get_url(host, media_id), patterns=[r'''file:\s*["'](?P<url>[^"']+)''']).replace(' ', '%20')
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.FF_USER_AGENT}
+        html = self.net.http_GET(web_url, headers=headers).content
+        sources = helpers.scrape_sources(html)
+        if sources:
+            return helpers.pick_source(sources) + helpers.append_headers(headers)
+        raise ResolverError('Video cannot be located.')
+
+    # def get_media_url(self, host, media_id):
+    #     return helpers.get_media_url(self.get_url(host, media_id),
+    #                                  patterns=['file:"(?P<url>[^"]+)",label:"(?P<label>[^"]+)'],
+    #                                  generic_patterns=False)
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id)
+        return self._default_get_url(host, media_id, template='https://hdvid.fun/embed-{media_id}.html')
--- lib/resolveurl/plugins/holavid.py
+++ lib/resolveurl/plugins/holavid.py
@@ -18,7 +18,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-import re
+
 from resolveurl.plugins.lib import helpers
 from resolveurl import common
 from resolveurl.resolver import ResolveUrl, ResolverError
@@ -36,16 +36,11 @@ class HolaVidResolver(ResolveUrl):
         web_url = self.get_url(host, media_id)
         headers = {'User-Agent': common.FF_USER_AGENT}
         html = self.net.http_GET(web_url, headers=headers).content
-
-        if html:
-            _srcs = re.search(r'sources\s*:\s*\[(.+?)\]', html)
-            if _srcs:
-                srcs = helpers.scrape_sources(_srcs.group(1), patterns=['''["'](?P<url>http[^"']+)'''], result_blacklist=['.m3u8'])
-                if srcs:
-                    headers.update({'Referer': web_url})
-                    return helpers.pick_source(srcs) + helpers.append_headers(headers)
-
-        raise ResolverError('Unable to locate link')
+        sources = helpers.scrape_sources(html)
+        if sources:
+            headers.update({'verifypeer': 'false'})
+            return helpers.pick_source(sources) + helpers.append_headers(headers)
+        raise ResolverError('Video cannot be located.')
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
